50bffc51acaf4e4433587e6f17134d6a80479c90
Iago <iagorechtramontin@gmail.com>
2024-09-04 20:27:25 -0300

    atividade de php

---
+++ atividade_01/index.php
@@ -0,0 +1,15 @@
+<?php 
+include 'includes/header.php';
+include 'includes/menu.php';
+?>
+
+<main>
+    <div>
+        <h1>Bem vindo</h1>
+        <p>Nossa empresa trabalha com transporte canino de animais de até 15kg na cidade de Joinville e região.</p>
+    </div>
+</main>
+
+<?php 
+include 'includes/footer.php';
+?>
